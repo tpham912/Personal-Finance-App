002515d08c23a93aa0a58e1bff97717b125766e9
Tina Pham <tina.pham8912@gmail.com>
2021-09-30 14:03:23 -0700

    got SW to work on local host

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
     "/",
     "/style.css",
@@ -11,7 +14,6 @@ const FILES_TO_CACHE = [
 
 self.addEventListener("install", function (evt) {
 
-    
     // pre cache all static assets
     evt.waitUntil(
       caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
