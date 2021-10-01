8b71140a53480aa82c5d6a8709bac95d9411917a
Tina Pham <tina.pham8912@gmail.com>
2021-10-01 14:34:20 -0700

    fixed server.js

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+  // useNewUrlParser: true,
+  // useFindAndModify: false
 });
 
 // routes
